27fdc8199708df563d88770fc47a52eb4814a3cf
pfernandolopes <53016713+pfernandolopes@users.noreply.github.com>
2026-05-02 16:18:28 -0300

    Criar sw.js

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = ‘sudoku-v6’;
+const ASSETS = [
+‘./index.html’,
+‘./manifest.json’,
+‘./icon.svg’
+];
+
+// Install: cache all assets
+self.addEventListener(‘install’, e => {
+e.waitUntil(
+caches.open(CACHE).then(c => c.addAll(ASSETS))
+);
+self.skipWaiting();
+});
+
+// Activate: delete old caches
+self.addEventListener(‘activate’, e => {
+e.waitUntil(
+caches.keys().then(keys =>
+Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+)
+);
+self.clients.claim();
+});
+
+// Fetch: cache-first strategy
+self.addEventListener(‘fetch’, e => {
+e.respondWith(
+caches.match(e.request).then(cached => cached || fetch(e.request))
+);
+});
